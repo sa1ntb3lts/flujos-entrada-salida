99e77191079f7e5fdaf53c439114ea6641c1ec81
sa1ntb3lts <229399078+sa1ntb3lts@users.noreply.github.com>
2026-09-13 13:41:13 -0700

    Implemented step 2 and 3 of GeneradorReporteIncidencias.java

---
+++ src/GeneradorReporteIncidencias.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.nio.file.*;
+
+public class GeneradorReporteIncidencias {
+
+    public static void main(String[] args) {
+
+        // 1. Validar argumentos
+        // 2. Crear Path del archivo de entrada
+        Path path = Path.of("mesa-ayuda/entrada", "incidencias.txt");
+        // 3. Comprobar existencia
+        System.out.println("El archivo existe?: " + Files.exists(path));
+        // 4. Mostrar información del archivo
+
+        // 5. Crear directorio de salida
+        // 6. Crear Paths para los archivos de salida
+
+        int total = 0;
+        int altas = 0;
+        int medias = 0;
+        int bajas = 0;
+
+        try (
+            BufferedReader lector = Files.newBufferedReader(/* completar */);
+            PrintWriter reporte = new PrintWriter(Files.newBufferedWriter(/* completar */));
+            PrintWriter reporteAltas = new PrintWriter(Files.newBufferedWriter(/* completar */))
+        ) {
+
+            String linea;
+
+            while ((linea = lector.readLine()) != null) {
+                // 7. Procesar cada incidencia
+                // 8. Clasificar prioridad
+                // 9. Escribir incidencias ALTA
+            }
+
+            // 10. Generar resumen
+
+        } catch (IOException e) {
+            System.err.println("Error de E/S: " + e.getMessage());
+        }
+    }
+}
