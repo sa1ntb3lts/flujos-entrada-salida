9562313d4aaa8e75d6a52faa497733d4e3c379e3
sa1ntb3lts <229399078+sa1ntb3lts@users.noreply.github.com>
2026-09-13 13:56:59 -0700

    Modified implementation of GeneradorReporteIncidencias.java

--- src/GeneradorReporteIncidencias.java
+++ src/GeneradorReporteIncidencias.java
@@ -15,7 +15,7 @@ public class GeneradorReporteIncidencias {
         System.out.println("Es archivo: " + Files.isRegularFile(path));
         System.out.println("Se puede leer: " + Files.isReadable(path));
         // 5. Crear directorio de salida
-        PrintWriter output = new PrintWriter(new FileWriter("salida/reportes"));
+        Path directorio = Files.createDirectories(Path.of("mesa-ayuda/salida/reportes"));
         // 6. Crear Paths para los archivos de salida
 
 
